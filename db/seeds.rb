889679c82f92ef33a5a70390f6a903fddad2523c
crespire <li.simmon@gmail.com>
2022-02-15 14:35:43 -0500

    Update seeds random to start from 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ gen_users.times do |i|
 end
 
 5.times do |i|
-  Event.create(name: "Test Event ##{i}", event_date: Date.new, published: [true, false].sample, max_guests: rand(50), creator_id: rand(gen_users))
+  Event.create(name: "Test Event ##{i}", event_date: Date.new, published: [true, false].sample, max_guests: rand(5..50), creator_id: rand(1..gen_users))
 end
