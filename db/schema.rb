2e2b8cf9110925b0e3f02bf065275e5ead619e69
crespire <li.simmon@gmail.com>
2022-02-16 10:52:41 -0500

    Update schema with migration to add foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_204746) do
+ActiveRecord::Schema.define(version: 2022_02_16_152823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2022_02_14_204746) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "published", default: false
     t.integer "creator_id"
-    t.integer "attendee_id"
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -48,4 +47,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_204746) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
+  add_foreign_key "invitations", "events"
+  add_foreign_key "invitations", "users", column: "attendee_id"
 end
